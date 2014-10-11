f2c851864f33360ce370cdbc902e12babc95e9a7
Varsha Vijayan <varsha.bme@gmail.com>
2014-10-11 15:23:48 -0700

    task_1_test.txt: converted character to hex & hex to binary.

--- task_2_cvt_ch_bytes.py
+++ task_2_cvt_ch_bytes.py
@@ -18,42 +18,20 @@ print f.read() # print the sentence
 
 #Q1 - how to identify encoding of string in file?
 
+scale = 16 ## equals to hexadecimal
+num_of_bits = 8
+
 # loop character-by-character
 with open('task_1_test.txt') as f:
     print "Character-by-character:"
     for line in f:
         for c in line:
             temp = bin(int(binascii.hexlify(c), 16))
-            print c, temp # printing character & hex representation
+            #binary_string = binascii.unhexlify(temp) # unhexlify works for even string lengths only
+            binary_string = bin(int(temp, scale))[2:].zfill(num_of_bits)
+            print c, binary_string # printing character & binary representation
             
-            
-            
-# ALL TRIALS
-#import array
-#import pip
-#import logging
-            #temp2=bytearray.fromhex(temp)
-            #temp2=binascii.unhexlify(temp)
-            # detect encoding
-#            def force_decode(c, codecs=['utf8', 'cp1252']):
-#                for i in codecs:
-#                    try:
-#                        return string.decode(i)
-#                        print c, string.decode(i)
-#                    except:
-#                        pass
-#
-#                logging.warn("cannot decode url %s" % ([c]))
 
-            #print c, bin(int(binascii.hexlify(c), 16)) # prints hexadecimal representation
-            #temp = bin(int(binascii.hexlify(c), 16))
-            #print array.array('B',c)
-            #print map(ord,c)
-            #print bytearray(c)
-            #print temp
-            #byte_list = map(ord, temp)
-            #print c, byte_list
-            #print bytearray.fromhex(temp)
             
             
             
